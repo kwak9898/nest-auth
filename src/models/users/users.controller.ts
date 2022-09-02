c47856cbec17235f944816e39453231a7ff3049b
kwak9898 <kwak9898@github.com>
2022-09-02 22:48:46 +0900

    users 구현중

--- src/models/users/users.controller.ts
+++ src/models/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { User } from '../../../entities/user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param() id: number): Promise<User> {
+    return this.usersService.findOne(id);
+  }
+
+  @Delete(':id')
+  async remove(@Param() id: string): Promise<void> {
+    return this.usersService.remove(id);
+  }
+}
--- src/models/users/users.service.ts
+++ src/models/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../../../entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  findOne(id: number): Promise<User> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+
+  async remove(id: string): Promise<void> {
+    await this.userRepository.delete(id);
+  }
+}
