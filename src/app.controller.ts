0a7316d11d9fec7e0187dc76720bbec0e3ad7dd9
kwak9898 <kwak9898@github.com>
2022-09-03 22:16:24 +0900

    controller constructor 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { LocalAuthGuard } from './guards/localAuth.guard';
+import { LocalAuthGuard } from './guards/auth/localAuth.guard';
 import { AuthService } from './models/auth/auth.service';
 
 @Controller()
