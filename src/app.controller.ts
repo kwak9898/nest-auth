750610a9d85fde462833aa5b575bec5d54d10de0
kwak9898 <kwak9898@github.com>
2022-09-03 21:42:10 +0900

    보류

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,8 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { AppService } from './app.service';
 import { LocalAuthGuard } from './guards/localAuth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
