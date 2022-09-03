cc7ebdf90e62b94577d1a9f1194c35939f4a5b2f
kwak9898 <kwak9898@github.com>
2022-09-03 23:13:10 +0900

    저장된 쿠키값의 토큰값이 올바른지 JWT를 사용했던 모든 곳에서 확인해야 하는 로직 구현

--- src/strategies/jwt.strategy.ts
+++ src/strategies/jwt.strategy.ts
@@ -5,7 +5,11 @@ import { jwtConstants } from '../models/auth/constants';
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // JWT 추출 방법을 제공 (Request의 Authorization 헤더에 토큰을 제공하는 방식.)
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        (request) => {
+          return request?.cookies?.Authenticator;
+        },
+      ]), // JWT 추출 방법을 제공 (Request의 Authorization 헤더에 토큰을 제공하는 방식.)
       ignoreExpiration: false, // false면 JWT가 만료되었는지 확인 후 만료 시 401 예외를 발생.
       secretOrKey: jwtConstants.secret, // 다칭키를 제공하는 옵션
     });
