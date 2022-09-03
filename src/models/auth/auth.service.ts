fc4d3e3bd3b50a52910f25c0ca48c4513347101e
kwak9898 <kwak9898@github.com>
2022-09-03 21:18:53 +0900

    유효성 검사 구현 중

--- src/models/auth/auth.service.ts
+++ src/models/auth/auth.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  // 비밀번호 유효성 검사
+  async validateUser(userName: string, password: string): Promise<any> {
+    const user = await this.usersService.findByUserName(userName);
+
+    if (user && user.password === password) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
--- src/models/users/users.controller.ts
+++ src/models/users/users.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { User } from '../../../entities/user.entity';
+import { User } from '../../entities/user.entity';
 
 @Controller('users')
 export class UsersController {
