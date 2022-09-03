23ef8ab85a51fa892a1ea59d637f49d91d64ca6f
kwak9898 <kwak9898@github.com>
2022-09-03 21:07:08 +0900

    특정 사용자 이름 찾는 함수 추가

--- src/models/users/users.service.ts
+++ src/models/users/users.service.ts
@@ -10,19 +10,28 @@ export class UsersService {
     private userRepository: Repository<User>,
   ) {}
 
+  // 모든 유저 찾기
   findAll(): Promise<User[]> {
     return this.userRepository.find();
   }
 
+  // 특정 유저 찾기
   findOne(id: number): Promise<User> {
     return this.userRepository.findOne({ where: { id } });
   }
 
+  // 유저 삭제
   async remove(id: string): Promise<void> {
     await this.userRepository.delete(id);
   }
 
+  // 유저 생성
   async createUser(user: User): Promise<void> {
     await this.userRepository.save(user);
+  }
+
+  // 특정 사용자 이름 찾기
+  async findByUserName(userName: string): Promise<User | undefined> {
+    return this.userRepository.findOne({ where: { userName } });
   }
 }
